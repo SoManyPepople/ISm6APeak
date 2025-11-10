59cb300bc48a2dd061ad011b186b676643dc475e
SoManyPeople <huangp@localhost.localdomain>
2025-11-10 14:54:27 +0800

    modify Rscript_MACS2

--- inst/scripts/Rscript_MACS2.R
+++ inst/scripts/Rscript_MACS2.R
@@ -77,7 +77,7 @@ Annot.gtf
 Annot.sqlite
 Annot.genome
 
-
+setwd(dir = outdir)
 if(SelectedCombo %in% dt.parameter.combo$ComboName){
   RunMACS2(MACS2_path = bin_path,
            org=ifelse(Organism=="Human","hs","mm"),
