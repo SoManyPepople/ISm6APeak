b465ae328a4c8240a707ca73217288dde9ce4613
SoManyPeople <huangp@localhost.localdomain>
2025-11-11 19:34:40 +0800

    add function

--- R/main_function.R
+++ R/main_function.R
@@ -266,6 +266,7 @@ runM6APeakS  <- function(
       foreach(j=c("Input","RIP"),.combine='rbind')%do%{
         dt.res <- data.table::fread(paste0(tmp.dir,"/",Samples[i],"_", j, "_infer.strandness.tsv"))
         print(dt.res)
+        print(str(dt.res))
         if(sum(str_detect(dt.res$V6,pattern=fixed("2++,2--")))>0){
           Library <- "PE"
           if(dt.res[str_detect(V6,fixed("2++,2--")),V7]>=0.5){Strandness <- "R2"}else{
